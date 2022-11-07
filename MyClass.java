d0d8ac8b07a4b935164b1994ec16410f1e20c577
Παναγιώτης Γουλιδάκης <32774035+pagoulid@users.noreply.github.com>
2022-11-07 19:30:29 +0200

    Update MyClass.java
    Updating project

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,6 @@
 import java.io.*;
 class MyClass{
   public static void main(String args[]){
-    System.out.println("This is the main function");
-    System.out.println("This is a copy of the main test-java project!");
+    System.out.println("Updating...")
   }
 }
